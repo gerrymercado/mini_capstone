6d39ef6fbbb7de8d5aaced27bbd6ebd0ddeb5343
gerry <gmercado6@gmail.com>
2018-05-27 14:58:53 -0700

    first_product commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def product_action
+    @product = Product.first 
+
+    render "product_view.json.jbuilder"
+  end 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(
+  name: "basketball",
+  price: 65,
+  image_url: "https://images.eastbay.com/pi/285507/zoom/nike-team-hyper-elite-basketball-mens",
+  description: "Nike Hyper-Elite Basketball"
+  )
+
+Product.create(
+  name: 
+  )
+
